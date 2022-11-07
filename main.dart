b0a8f897b08dada146d37746301d4caecd0293c1
DouglasCFarias <108027630+DouglasCFarias@users.noreply.github.com>
2022-11-07 18:13:18 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,19 +2,17 @@ import 'package:flutter/material.dart';
 import 'LoginPage.dart';
 
 void main() {
-  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) :super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context){
-  return MaterialApp(
-
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(),
-  );
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
