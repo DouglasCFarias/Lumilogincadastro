d71e949461501e1c6024aef75f73c95c2a6645eb
DouglasCFarias <108027630+DouglasCFarias@users.noreply.github.com>
2022-11-07 18:10:13 -0300

    Update SingInPagee.dart

--- SingInPagee.dart
+++ SingInPagee.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
 import 'EmailCodePagee.dart';
+
 class SingInPagee extends StatefulWidget {
   const SingInPagee({super.key});
 
@@ -12,76 +13,96 @@ class _SingInPageeState extends State<SingInPagee> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 70, 29, 77),
-      body: 
-      Column(
-      children: [
-      //Lumi
+        backgroundColor: Color(0xff220037),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          //Lumi
+          SizedBox(
+            height: 20,
+          ),
+          Image.asset(
+            'assets/lumi.png',
+            width: 292,
+          ),
+          //email
+          SizedBox(height: 70),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+            ),
+          ),
+          //buton 1
+          SizedBox(
+            height: 70,
+          ),
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(249, 88, 9, 246),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: MaterialButton(
+                    minWidth: 100,
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return EmailCodePagee();
+                      }));
+                    },
+                    child: Text(
+                      'Avançar',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              )),
+          //buton
+          SizedBox(
+            height: 200,
+          ),
 
-      //email
-     Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Container(
-        decoration: BoxDecoration(
-          color:Colors.grey,
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-     ),
-      //buton 1
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-   child: 
-   Container(
-    decoration: 
-    BoxDecoration(
-      color: Colors.lightBlueAccent,
-      borderRadius: BorderRadius.circular(12),
-      ),
-    child:
-   GestureDetector(onTap: (){
-   Navigator.push(context, MaterialPageRoute(builder: (context){
-    return EmailCodePagee();
-   }));
-},
-child: Text('Avançar'),
- ),)
- ),
-      //buton 
-SizedBox(height: 120,),
-
-Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 25),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-      Text(
-        "Já tem conta?",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.grey,
-        )
-        ),
-        Padding(padding: const EdgeInsets.symmetric(horizontal: 25)),
-        
-        GestureDetector(
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return LoginPage();
-            }));
-          },
-          child: Text(
-        "Entrar",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
-        ),
-        )
-      ]
-    )
-  ),
-     ] )
-    );
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text("Já tem conta?",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    )),
+                Padding(padding: const EdgeInsets.symmetric(horizontal: 2)),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return LoginPage();
+                    }));
+                  },
+                  child: Text(
+                    "Entrar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ])),
+        ])));
   }
 }
