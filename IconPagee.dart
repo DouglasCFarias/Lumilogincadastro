15274cb2a4d86f1d03adfd5be4f97de34df53ae4
DouglasCFarias <108027630+DouglasCFarias@users.noreply.github.com>
2022-11-07 18:15:03 -0300

    Update IconPagee.dart

--- IconPagee.dart
+++ IconPagee.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class IconPagee extends StatefulWidget {
   const IconPagee({super.key});
 
@@ -10,88 +11,86 @@ class _IconPageeState extends State<IconPagee> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      backgroundColor: Colors.purple,
-      body:
-      
-      Column(children: [
-    SizedBox(height: 50,),
-      //Icon: nao encontrei um igual ao do figma ainda
-      //Padding(
-      //padding: const EdgeInsets.symmetric(horizontal:25),
-     // child:
-     // Icon(
-        //IconData(0xee47, fontFamily: 'MaterialIcons'),
-    //  ),
-   //   ),
+        backgroundColor: Color(0xff220037),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            //Icon: nao encontrei um igual ao do figma ainda
+            //Padding(
+            //padding: const EdgeInsets.symmetric(horizontal:25),
+            // child:
+            // Icon(
+            //IconData(0xee47, fontFamily: 'MaterialIcons'),
+            //  ),
+            //   ),
 
+            //Text icon
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Text(
+                'Adicionar foto do perfil',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            //Text 1 icon
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Text(
+                  'Adicione uma foto do perfil para que seus amigos saibam que é você',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            SizedBox(
+              height: 10,
+            ),
+            //buton 1
 
-     //Text icon
-     Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child: Text( 'Adicionar foto do perfil',
-        style: 
-
-        TextStyle(
-          color:Colors. white, fontWeight: FontWeight.bold
-          ),
-      ),
-     ),
-     SizedBox(height: 10,),
-     //Text 1 icon
-     Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      
-      child:
-      Text('Adicione uma foto do perfil para que seus amigos saibam que é você',
-      style: TextStyle(
-        color:Colors.white,
-        ),
-      )
-     ),
-     SizedBox(height: 10,),
-     //buton 1
-     
-     Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
-      child:
-      Container(
-        decoration: BoxDecoration(
-          color:Colors.lightBlueAccent,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: 
-        MaterialButton(onPressed: (){
-          setState(() {
-          });
-        },
-        child: 
-        Text(
-          'Adicionar uma foto',style: TextStyle(color:Colors.white),)
-        ),
-      ),
-     ),
-SizedBox(height:10),
-     // buton 2
-     Padding(padding: const EdgeInsets.symmetric(horizontal: 25),
-     child: Container(
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-      ),
-      child: MaterialButton(onPressed: (){
-        setState(() {
-        });
-      },
-      child: Text('Pular',
-      style: TextStyle(
-        color:Colors.deepPurple,
-        ),
-        ),
-     ),
-     ),
-     ),
-      ],
-      )
-    );
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: MaterialButton(
+                    onPressed: () {
+                      setState(() {});
+                    },
+                    child: Text(
+                      'Adicionar uma foto',
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+            ),
+            SizedBox(height: 10),
+            // buton 2
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: MaterialButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text(
+                    'Pular',
+                    style: TextStyle(
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
